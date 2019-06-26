954eb350def8991f4c5b35a199490178116a38c0
Richard_Yi <yyf_richard@outlook.com>
2019-06-26 18:19:21 +0800

    submit test case file for SynchronizedClassOrStaticMethodExample.java 学习要点： 1. 同步类和同步静态方法是等价的，因为静态方法是和类绑定的。 2. 同步类 线程会获得 类级别锁，其他线程进入阻塞状态，无法访问这个被锁住的类的静态变量
    TODO：有疑问暂未解决。

---
+++ src/lock/SynchronizedClassOrStaticMethodExample.java
@@ -0,0 +1,78 @@
+package lock;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 同一个类 或者 同步一个静态方法
+ * @author Richard_yyf
+ * @version 1.0 2019/6/26
+ */
+public class SynchronizedClassOrStaticMethodExample {
+
+    private static final int TIMES = 10;
+
+    private final static int param = 1231;
+
+    public void func1() {
+
+        synchronized (SynchronizedClassOrStaticMethodExample.class) {
+            for (int i = 0; i < TIMES; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println("func1 finished");
+        }
+    }
+
+    public synchronized static void synchronizedStaticMethod() {
+
+        for (int i = 0; i < TIMES; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("synchronizedStaticMethod finished");
+
+    }
+
+    public static void func3() {
+        for (int i = 0; i < TIMES; i++) {
+            System.out.println(i + " " + param + " ");
+        }
+        System.out.println("func3 finished");
+    }
+
+    public void func4() {
+        for (int i = 0; i < TIMES; i++) {
+            System.out.println(i + " " + "func4" + " ");
+        }
+        System.out.println("func4 finished");
+    }
+
+
+    public static void main(String[] args) {
+//        case1();
+        case2();
+    }
+
+    private static void case1() {
+        SynchronizedClassOrStaticMethodExample example1 = new SynchronizedClassOrStaticMethodExample();
+        SynchronizedClassOrStaticMethodExample example2 = new SynchronizedClassOrStaticMethodExample();
+        ExecutorService exector = Executors.newCachedThreadPool();
+        // 往线程池中放两个任务 按照 缓存线程池的机制，会有两个线程执行
+        exector.execute(example1::func1);
+        exector.execute(example2::func1);
+//        exector.execute(example::func2);
+    }
+
+    private static void case2() {
+        SynchronizedClassOrStaticMethodExample example = new SynchronizedClassOrStaticMethodExample();
+        ExecutorService exector = Executors.newCachedThreadPool();
+        // 往线程池中放两个任务 按照 缓存线程池的机制，会有两个线程执行
+        exector.execute(SynchronizedClassOrStaticMethodExample::synchronizedStaticMethod);
+//        exector.execute(SynchronizedClassOrStaticMethodExample::synchronizedStaticMethod);
+        exector.execute(example::func4);
+    }
+
+    //  为什么这样执行case2 会造成两个线程wait
+    //  exector.execute(SynchronizedClassOrStaticMethodExample::synchronizedStaticMethod);
+    //  exector.execute(example::func4);
+}
