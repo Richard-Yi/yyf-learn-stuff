0a804857b5f1dbe7c9d5bfb95d232be94d6c0c88
Richard_Yi <yyf_richard@outlook.com>
2019-06-27 09:56:54 +0800

    [update] test case file for ReentrantLockExample 1. 增加 tryLock() 等待可中断 场景示例

--- src/lock/ReentrantLockExample.java
+++ src/lock/ReentrantLockExample.java
@@ -2,6 +2,7 @@ package lock;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -28,9 +29,70 @@ public class ReentrantLockExample {
         }
     }
 
+    public void sleepFunc1() {
+        lock.lock();
+        try {
+            for (int i = 0; i < TIMES; i++) {
+                System.out.print(i + " ");
+            }
+            System.out.println(Thread.currentThread().getName() + " be in sleep 10s");
+            try {
+                Thread.sleep(10 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * ReentrantLock  Lock的高级应用，当持有锁的线程长期不释放锁的时候，正在等待的线程可以选择放弃等待，改为处理其他事情
+     */
+    public void func2() {
+        boolean isLocked = false;
+        try {
+            try {
+                // 超时返回false
+                // 成功获取到临界区的锁 返回 true
+                isLocked = lock.tryLock(1000, TimeUnit.MILLISECONDS);
+            } catch (InterruptedException e) {
+                // 当被其他线程中断时抛出异常
+                e.printStackTrace();
+            }
+            if (isLocked) {
+                // .... specific logic here
+                System.out.println(Thread.currentThread().getName() + " succeed to get lock now");
+                for (int i = 0; i < TIMES; i++) {
+                    System.out.print(i + " ");
+                }
+                // ....
+            } else {
+                System.out.println(Thread.currentThread().getName() + " fail get lock now");
+            }
+        } finally {
+            if (isLocked) {
+                lock.unlock();
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-        case1();
-//        case2();
+//        case1();
+        case2();
+    }
+
+    /**
+     * Lock 用法中的 "等待可中断" 场景
+     */
+    private static void case2() {
+        ReentrantLockExample example = new ReentrantLockExample();
+        ExecutorService exector = Executors.newCachedThreadPool();
+        exector.execute(example::sleepFunc1);
+        exector.execute(example::func2);
+
+        ThreadPoolUtil.tryReleasePool(exector);
     }
 
     /**
@@ -44,6 +106,7 @@ public class ReentrantLockExample {
 
         ThreadPoolUtil.tryReleasePool(exector);
     }
+
 
 
 }
