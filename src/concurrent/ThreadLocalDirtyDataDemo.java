a9051aa15bbd907fd8292692da2d376a164a389f
Richard_Yi <yyf_richard@outlook.com>
2019-07-05 08:52:39 +0800

    [add] test case file for ThreadLocalDirtyDataDemo.java 1. 模拟 ThreadLocal 造成脏数据的场景

---
+++ src/concurrent/ThreadLocalDirtyDataDemo.java
@@ -0,0 +1,41 @@
+package concurrent;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * ThreadLocal 造成脏数据的场景
+ *
+ * @author Richard_yyf
+ * @version 1.0 2019/7/5
+ */
+public class ThreadLocalDirtyDataDemo {
+
+    public static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+
+    public static void main(String[] args) {
+
+        ExecutorService pool = Executors.newFixedThreadPool(1);
+
+        for (int i = 0; i < 2; i++) {
+            MyThread thread = new MyThread();
+            pool.execute(thread);
+        }
+    }
+
+    private static class MyThread extends Thread {
+        private static boolean flag = true;
+
+        @Override
+        public void run() {
+            if (flag) {
+                // 第一个线程set之后，并没有进行remove
+                // 而第二个线程由于某种原因(这里是flag=false) 没有进行set操作
+                threadLocal.set(this.getName() + ", session info ");
+                flag = false;
+            }
+            System.out.println(this.getName() + " 线程 是 " + threadLocal.get());
+            // 线程使用完threadLocal，要及时remove
+        }
+    }
+}
