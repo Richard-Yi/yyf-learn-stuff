70f73b31b59ce637999af2b25569aac76d70c2bd
Richard_Yi <yyf_richard@outlook.com>
2019-07-08 19:02:21 +0800

    [add] 1. 方法重载的most specific 原则

---
+++ src/oop/MethodOverLoadConfusingDemo.java
@@ -0,0 +1,29 @@
+package oop;
+
+/**
+ * The constructor Confusing(Object)accepts any parameter passed to Confusing(double[]),
+ * so Confusing(Object) is less specific.
+ * (Every double array is an Object, but not every Object is a double array.)
+ * The most specific constructor is therefore Confusing(double[]), which explains the program's output.
+ *
+ * @author Richard_yyf
+ * @version 1.0 2019/7/8
+ */
+public class MethodOverLoadConfusingDemo {
+
+    public static void confuse(Object o) {
+        System.out.println("Object");
+    }
+    public static void confuse(double[] dArray) {
+        System.out.println("double array");
+    }
+//    public static void confuse(int[] dArray) {
+//        System.out.println("int array");
+//    }
+    public static void confuse(int dArray) {
+        System.out.println("int array");
+    }
+    public static void main(String args[]) {
+        confuse(null);
+    }
+}
