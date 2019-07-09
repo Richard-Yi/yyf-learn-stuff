160096be0719bef543a599cc45ee8840e497e6a3
Richard_Yi <yyf_richard@outlook.com>
2019-07-09 08:40:18 +0800

    [add] 1. 增加学习注释

--- src/collections/HashSetDemo.java
+++ src/collections/HashSetDemo.java
@@ -10,8 +10,14 @@ public class HashSetDemo {
 
     public static void main(String[] args) {
         HashSet<Object> hashSet = new HashSet<>();
+        // class A 的 equal() 总是true hashCode() 方法没有重写
+        // 会导致HashSet把两个对象保存在Hash表的不同位置，从而使两个equals()=true的对象都添加成功
         hashSet.add(new A());
         hashSet.add(new A());
+        // class B 的 hashCode() 总是1
+        // equal() 方法没有重写
+        // 更麻烦的情况，hashCode()方法返回相同，HashSet会尝试保存这两个对象到同一个位置，
+        // 但是又不行，所以这个位置实际上会使用链式结构存储多个对象，这样子会导致HashSet的性能下降
         hashSet.add(new B());
         hashSet.add(new B());
         hashSet.add(new C());
@@ -30,7 +36,7 @@ class A {
         return true;
     }
 
-    
+
 }
 
 
