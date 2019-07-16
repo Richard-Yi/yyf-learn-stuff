59561de5e56c969abb290f52ac39f21018861df3
Richard_Yi <yyf_richard@outlook.com>
2019-07-16 14:11:26 +0800

    [add] 1. StreamFilterDemo 更新

--- src/lambda/StreamFilterDemo.java
+++ src/lambda/StreamFilterDemo.java
@@ -1,8 +1,11 @@
 package lambda;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -17,27 +20,28 @@ public class StreamFilterDemo {
         List<Person> personList = new ArrayList<>();
         Person p1 = new Person("xiaoming",1);
         Person p2 = new Person("xiaoming",2);
-        Person p5 = new Person("xiaoming",5);
+//        Person p5 = new Person("xiaoming",5);
         Person p3 = new Person("xiaoxin",4);
         Person p4 = new Person("xiaoxin",5);
-        personList.add(p4);
-        personList.add(p5);
         personList.add(p1);
-        personList.add(p3);
         personList.add(p2);
+        personList.add(p3);
+        personList.add(p4);
 
         // 去重
         List<Person> resultList = personList.stream()
-                .filter(distinctByName(Person::getName))
                 .sorted((h1, h2) -> h2.getAge() - h1.getAge())
+                .filter(distinctByName(Person::getName))
+//                .filter(distinctByNameAndAge(Person::getName, Person::getAge))
                 .collect(Collectors.toList());
 
-        List<Person> resultList2 = personList.stream()
-                .distinct()
-                .sorted((h1, h2) -> h2.getAge() - h1.getAge())
-                .collect(Collectors.toList());
+//        List<Person> resultList2 = personList.stream()
+//                .distinct()
+//                .sorted((h1, h2) -> h2.getAge() - h1.getAge())
+//                .collect(Collectors.toList());
+        System.out.println(personList);
         System.out.println(resultList);
-        System.out.println(resultList2);
+//        System.out.println(resultList2);
 
         // 选出最大的
 //        Person oldest = resultList.get(0);
@@ -49,11 +53,27 @@ public class StreamFilterDemo {
     }
 
 
+    public static Predicate<Person> distinctByNameAndAge(Function<Person, String> keyExtractor, Function<Person, Integer> valExtractor) {
+        ConcurrentMap<String, Integer> map = new ConcurrentHashMap<>(3);
+        return p -> {
+            String key = keyExtractor.apply(p);
+            Integer val = valExtractor.apply(p);
+            Integer oldVal = map.get(key);
+            if (oldVal == null) {
+                map.put(key, val);
+                return true;
+            } else if (oldVal < val) {
+                map.put(key, val);
+                return true;
+            }
+            return false;
+        };
+    }
+
     public static <T> Predicate<T> distinctByName(Function<? super T, ?> keyExtractor) {
         Set<Object> seen = ConcurrentHashMap.newKeySet();
         return t -> seen.add(keyExtractor.apply(t));
     }
-
 
 
     private static class Person implements Serializable {
@@ -88,18 +108,6 @@ public class StreamFilterDemo {
                     '}';
         }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof Person) {
-                 return ((Person) obj).getName().equals(this.getName());
-            }
-            return super.equals(obj);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(name);
-        }
     }
 
 
